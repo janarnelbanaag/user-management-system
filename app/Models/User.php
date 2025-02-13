37db0be486f5cf2f62f8ca82119cdad5d8dac6c4
Jan Arnel Banaag <jhonarnel476@gmail.com>
2025-02-13 12:12:35 +0800

    added fillable, hidden, and casts for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        "first_name",
+        "last_name",
+        "address",
+        "postcode",
+        "phone_number",
+        "email",
+        "username",
+        "password",
+        "role"
+    ];
+
+    protected $hidden = [
+        "password"
+    ];
+
+    protected $casts = [
+        "password" => "hashed"
+    ];
 }
