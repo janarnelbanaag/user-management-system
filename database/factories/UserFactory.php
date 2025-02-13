260c8717207b47f8016c5b8485f61595134c9bba
Jan Arnel Banaag <jhonarnel476@gmail.com>
2025-02-13 14:49:14 +0800

    User factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,11 +22,26 @@ class UserFactory extends Factory
             'last_name' => $this->faker->lastName(),
             'address' => $this->faker->address(),
             'postcode' => $this->faker->postcode(),
-            'contact_phone' => $this->faker->phoneNumber(),
+            'phone_number' => $this->faker->phoneNumber(),
             'email' => $this->faker->unique()->safeEmail(),
             'username' => $this->faker->unique()->userName(),
             'password' => Hash::make('password123'),
             'role' => 'user',
         ];
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'first_name' => 'admin', 
+            'last_name' => '1', 
+            'address' => '1', 
+            'postcode' => '1', 
+            'phone_number' => '1', 
+            'email' => 'admin@email.com', 
+            'username' => 'admin', 
+            'password' => 'password', 
+            'role' => 'admin'
+        ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->count(1)->create(['role' => 'admin']);
+        User::factory()->admin()->create();
         User::factory(10)->create();
     }
 }
